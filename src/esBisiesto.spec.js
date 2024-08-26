95bbbeb81e7d988b21fa917fbd782f7b162cc6d0
Jherson <Jherson.chambi@ucb.edu.bo>
2024-08-27 01:25:10 +0200

    Segunda version todos los anios divisibles por 100 pero no por 400 del anio 2008

--- src/esBisiesto.spec.js
+++ src/esBisiesto.spec.js
@@ -9,5 +9,9 @@ describe("Es bisiesto", () => {
     const result = esBisiesto(1700)
     expect(result).toEqual(false);
   });
+  it("Todos los años divisibles por 100 pero no por 400 NO son bisiestos", () => {
+    const result = esBisiesto(2008)
+    expect(result).toEqual(true);
+  });
 });
 
