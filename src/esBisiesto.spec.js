5695fa4943bb902a1fe1b821abaf05391de46bde
Jherson <Jherson.chambi@ucb.edu.bo>
2024-08-27 01:19:22 +0200

    Segunda version todos los anios divisibles por 100 pero no por 400 no son bisiesto

---
+++ src/esBisiesto.spec.js
@@ -0,0 +1,13 @@
+import esBisiesto from "./esBisiesto.js";
+
+describe("Es bisiesto", () => {
+  it("Todo numero divisible entre 400 son bisiestos", () => {
+    const result = esBisiesto(2000)
+    expect(result).toEqual(true);
+  });
+  it("Todos los años divisibles por 100 pero no por 400 NO son bisiestos", () => {
+    const result = esBisiesto(1700)
+    expect(result).toEqual(false);
+  });
+});
+
